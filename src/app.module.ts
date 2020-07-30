195ef532f80f3c22515832651081b1fbd33be4bf
hovanvydut <hovanvydut@gmail.com>
2020-07-30 20:35:24 +0700

    add CACHE into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
@@ -9,11 +9,13 @@ import { BlogModule } from './blog/blog.module';
 import { ContactModule } from './contact/contact.module';
 import { CampaignModule } from './campaign/campaign.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 // import { ConfigModule } from '@nestjs/config';
 // import configuration from './config/configuration';
 
 @Module({
   imports: [
+    CacheModule.register(),
     TypeOrmModule.forRoot(typeOrmConfig),
     // ConfigModule.forRoot({
     //   isGlobal: true,
@@ -28,6 +30,12 @@ import { typeOrmConfig } from './config/typeorm.config';
     BlogModule,
     ContactModule,
     CampaignModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
